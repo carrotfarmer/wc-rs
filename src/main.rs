cdf392e84dd558c6e3a73939f781787741318219
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-05-17 17:12:46 +0530

    feat: read file contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::{File, self};
+use std::io::ErrorKind;
+use std::process::exit;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    filename: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match File::open(&args.filename) {
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => {
+                println!("No such file: {:?}", args.filename);
+                exit(1);
+            },
+            _ => {
+                println!("Error: {:?}", e);
+                exit(1);
+            }
+        },
+        Ok(_) => {}
+    }
+
+    let file_contents = fs::read_to_string(&args.filename).unwrap();
+
+    println!("{}", file_contents);
 }

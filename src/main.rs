df0442e583f2f45c184d9cc61b90c6ef5fe9d045
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-05-17 21:20:34 +0530

    feat: multiple files

--- src/main.rs
+++ src/main.rs
@@ -64,30 +64,70 @@ impl Display for Wc {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "\t")?;
 
+        // for max_line_len flag
+        let mut longest = String::from("");
+
+        let mut total_wc = 0;
+        let mut total_lines = 0;
+        let mut total_bytes = 0;
+        let mut total_chars = 0;
+
         for filename in &self.filenames {
             let contents = fs::read_to_string(&filename).unwrap();
 
             if self.lines {
+                total_lines += count_lines(&contents);
                 write!(f, "{}\t", count_lines(&contents))?;
             }
 
             if self.words {
+                total_wc += count_words(&contents);
                 write!(f, "{}\t", count_words(&contents))?;
             }
 
             if self.bytes {
+                total_bytes += count_bytes(&contents);
                 write!(f, "{}\t", count_bytes(&contents))?;
             }
 
             if self.chars {
+                total_chars += count_chars(&contents);
                 write!(f, "{}\t", count_chars(&contents))?;
             }
 
             if self.max_line_length {
-                write!(f, "{} ", max_line_len(&contents))?;
+                if max_line_len(&contents) > max_line_len(&longest) {
+                    longest = String::from(&contents);
+                }
+
+                write!(f, "{}", max_line_len(&contents))?;
             }
 
             write!(f, "{}\n\t", &filename.display())?
+        }
+
+        if self.filenames.len() > 1 {
+            if self.lines {
+                write!(f, "{}\t", total_lines)?
+            }
+
+            if self.words {
+                write!(f, "{}\t", total_wc)?
+            }
+
+            if self.bytes {
+                write!(f, "{}\t", total_bytes)?
+            }
+
+            if self.chars {
+                write!(f, "{}\t", total_chars)?
+            }
+
+            if self.max_line_length {
+                write!(f, "{}", max_line_len(&longest))?
+            }
+
+            write!(f, "total")?
         }
 
         Ok(())
